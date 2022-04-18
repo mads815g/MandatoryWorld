d4dbd13c70f45b6fd77cb921774f29372d624899
mads815g <mads61096@gmail.com>
2022-04-18 21:26:05 +0200

    public world

--- MandatoryWorld/World.cs
+++ MandatoryWorld/World.cs
@@ -4,7 +4,7 @@ namespace MandatoryWorld
     /// <summary>
     /// The World class, where the size of the world is set with y and x attributes.
     /// </summary>
-    static class World
+    public static class World
     {
         public static int MaxY;
         public static int MaxX;
